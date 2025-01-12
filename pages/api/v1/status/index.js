0ce7284337c9347164360a6f9973357d73a452da
Douglas Campos <60862078+Douglascrc@users.noreply.github.com>
2025-01-12 23:50:26 +0000

    feat: finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,25 +3,31 @@ import database from "infra/database";
 async function status(request, response) {
   let updatedAt = new Date().toISOString();
   let databaseSettings = {};
-
+  const databaseName = process.env.POSTGRES_DB;
   try {
-    let databaseResult = await database.query(
-      `SELECT current_setting('server_version') AS version, 
-      current_setting('max_connections') AS max_connections,
-      COUNT(*) AS used_connections
-      FROM pg_stat_activity`,
-    );
+    let databaseResult = await database.query({
+      text: `SELECT current_setting('server_version')::FLOAT AS version, 
+      current_setting('max_connections')::INT AS max_connections,
+      COUNT(*)::INT AS used_connections
+      FROM pg_stat_activity WHERE datname = $1;`,
+      values: [databaseName],
+    });
 
-    databaseSettings = databaseResult.rows[0];
+    databaseSettings = {
+      database_status: "healthy",
+      ...databaseResult.rows[0],
+    };
   } catch (error) {
     databaseSettings = {
-      databaseStatus: "Disconnected",
+      database_status: "unhealthy",
     };
   }
 
   response.status(200).json({
     updated_at: updatedAt,
-    database_settings: databaseSettings,
+    dependencies: {
+      database_info: databaseSettings,
+    },
   });
 }
 
