6ef59045deb51d1cff891a1aa4961c8d0bb63541
Douglas Campos <60862078+Douglascrc@users.noreply.github.com>
2025-01-12 23:51:09 +0000

    test: database's healthy

--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -8,12 +8,31 @@ test("GET localhost:3000/api/v1/status should return 200", async () => {
   expect(responseBody.updated_at).toBeDefined();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.database_settings.max_connections).toBeDefined();
-  expect(typeof responseBody.database_settings.max_connections).toBe("number");
+  expect(responseBody.dependencies.database_info.database_status).toBeDefined();
+  expect(typeof responseBody.dependencies.database_info.database_status).toBe(
+    "string",
+  );
 
-  expect(responseBody.database_settings.used_connections).toBeDefined();
-  expect(typeof responseBody.database_settings.used_connections).toBe("number");
+  expect(responseBody.dependencies.database_info.max_connections).toBeDefined();
+  expect(typeof responseBody.dependencies.database_info.max_connections).toBe(
+    "number",
+  );
 
-  expect(responseBody.database_settings.version).toBeDefined();
-  expect(typeof responseBody.database_settings.version).toBe("number");
+  expect(
+    responseBody.dependencies.database_info.used_connections,
+  ).toBeDefined();
+  expect(typeof responseBody.dependencies.database_info.used_connections).toBe(
+    "number",
+  );
+
+  expect(responseBody.dependencies.database_info.version).toBeDefined();
+  expect(typeof responseBody.dependencies.database_info.version).toBe("number");
+});
+
+test("should return database_status: 'unhealthy' ", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.database_info.database_status).toBe("unhealthy");
 });
