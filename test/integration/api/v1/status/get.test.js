0a439ecf988e9c74a2131f7ff366cae6f4ab7443
Douglas Campos <60862078+Douglascrc@users.noreply.github.com>
2025-01-12 16:13:00 +0000

    test: status endpoint

--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,4 +1,19 @@
 test("GET localhost:3000/api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.database_settings.max_connections).toBeDefined();
+  expect(typeof responseBody.database_settings.max_connections).toBe("number");
+
+  expect(responseBody.database_settings.used_connections).toBeDefined();
+  expect(typeof responseBody.database_settings.used_connections).toBe("number");
+
+  expect(responseBody.database_settings.version).toBeDefined();
+  expect(typeof responseBody.database_settings.version).toBe("number");
 });
