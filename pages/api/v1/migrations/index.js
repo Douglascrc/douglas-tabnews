6eebe49d046691348217799ef55604e50dd80719
Douglas Campos <60862078+Douglascrc@users.noreply.github.com>
2025-03-06 23:30:19 +0000

    feat: implement router for migrations API with error handling for unsupported methods

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,9 +1,35 @@
 import migrationsRunner from "node-pg-migrate";
 import { join } from "node:path";
 import database from "infra/database";
+import { InternalServerError, MethodNotAllowedError } from "infra/errors";
+import { createRouter } from "next-connect";
 
-export default async function migrations(request, response) {
+const router = createRouter();
+
+router.get(migrations);
+router.post(migrations);
+
+export default router.handler({
+  onNoMatch: onNoMatchHandler,
+  onError: onErrorHandler,
+});
+
+function onErrorHandler(error, request, response) {
+  const publicErrorObject = new InternalServerError({
+    cause: error,
+  });
+  console.error(publicErrorObject);
+  response.status(500).json(publicErrorObject);
+}
+
+function onNoMatchHandler(request, response) {
+  const publicErrorObject = new MethodNotAllowedError();
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+async function migrations(request, response) {
   let dbClient;
+
   try {
     dbClient = await database.getNewClient();
 
---
+++ tests/integration/api/v1/migrations/methods.test.js
@@ -0,0 +1,28 @@
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitAllServices();
+  await orchestrator.clearDatabase();
+});
+
+describe("Forbbiden methods /api/v1/migrations", () => {
+  const blockedMethods = ["PUT", "DELETE", "PATCH", "OPTIONS"];
+
+  test.each(blockedMethods)("Methods should be blocked", async (method) => {
+    const response = await fetch(
+      `${process.env.WEB_SERVER}/api//v1/migrations`,
+      {
+        method,
+      },
+    );
+    expect(response.status).toBe(405);
+
+    const responseBody = await response.json();
+    expect(responseBody).toEqual({
+      name: "Method Not Allowed Error",
+      message: "Método não permitido para esse endpoint",
+      action: "Verifique se o método HTTP é válido para esse endpoint",
+      status_code: 405,
+    });
+  });
+});
