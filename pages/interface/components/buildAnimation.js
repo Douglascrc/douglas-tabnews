131857a555a43aa40eff6dc3537c7c2b2d98bad0
Douglas Campos <60862078+Douglascrc@users.noreply.github.com>
2025-03-12 20:20:19 +0000

    feat: add animated crane component to home page

---
+++ pages/interface/components/buildAnimation.js
@@ -0,0 +1,42 @@
+import styles from "../styles/buildAnimation.module.css";
+
+function BuildAnimation() {
+  return (
+    <div className={styles.crane}>
+      <div className={styles.crane_jib}>
+        <span></span>
+        <span></span>
+      </div>
+      <div className={styles.crane_jib_head}></div>
+      <div className={styles.crane_jib_head_rope}>
+        <div></div>
+        <div>
+          <span className={styles.crane_jib_head_hook}></span>
+        </div>
+      </div>
+      <div className={styles.crane_body}>
+        <div className={styles.crane_body_cabin}>
+          <div className={styles.crane_body_cabin_door}></div>
+        </div>
+        <div className={styles.crane_body_engine}>
+          <div>
+            <span>0</span>
+          </div>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div className={styles.crane_caterpillar}>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
+}
+
+export default BuildAnimation;
