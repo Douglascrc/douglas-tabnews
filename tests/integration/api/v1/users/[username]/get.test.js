e5a137a3f6f4c082972113e605145adb2d93f6a4
Douglas Campos <60862078+Douglascrc@users.noreply.github.com>
2025-05-25 20:31:26 +0000

    feat: add endpoint to retrieve user by username

---
+++ tests/integration/api/v1/users/[username]/get.test.js
@@ -0,0 +1,75 @@
+import { version as uuidVersion } from "uuid";
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+
+  await fetch(`${process.env.WEB_SERVER}/api/v1/users`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: "douglasCampos",
+      email: "douglas@mail.com",
+      password: "1234",
+    }),
+  });
+});
+
+describe("GET /api/v1/users/[username]", () => {
+  describe("Anonymous user", () => {
+    test("With exact case match", async () => {
+      const response = await fetch(
+        `${process.env.WEB_SERVER}/api/v1/users/douglasCampos`,
+      );
+
+      expect(response.status).toBe(200);
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        id: responseBody.id,
+        username: responseBody.username,
+        email: responseBody.email,
+        password: responseBody.password,
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
+
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+
+      const response2 = await fetch(
+        `${process.env.WEB_SERVER}/api/v1/users/douglasCAmpos`,
+      );
+      expect(response2.status).toBe(200);
+      const response2Body = await response2.json();
+
+      expect(response2Body).toEqual({
+        id: response2Body.id,
+        username: response2Body.username,
+        email: response2Body.email,
+        password: response2Body.password,
+        created_at: response2Body.created_at,
+        updated_at: response2Body.updated_at,
+      });
+    });
+
+    test("With a mismatch", async () => {
+      const response = await fetch(
+        `${process.env.WEB_SERVER}/api/v1/users/caio`,
+      );
+
+      expect(response.status).toBe(404);
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "NotFoundError",
+        message: "O username não foi encontrado no sistema.",
+        action: "Verifique se o username está digitado corretamente.",
+        status_code: 404,
+      });
+    });
+  });
+});
